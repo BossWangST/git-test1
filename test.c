50875ac50b5632de4f2f1b40879c37dfa67a7cc6
BossWangST <bosswang1226@gmail.com>
2022-05-24 23:22:28 +0800

    add print to test.c

--- test.c
+++ test.c
@@ -3,6 +3,11 @@
 int main()
 {
     printf("Hello world\n");
+    printf("HAHA, I'm master-copy!!!!!\n");
+    printf("HAHA, I'm master-copy!!!!!\n");
+    printf("HAHA, I'm master-copy!!!!!\n");
+    printf("HAHA, I'm master-copy!!!!!\n");
+    printf("HAHA, I'm master-copy!!!!!\n");
     return 0;
 }
 
